de4f23e19201819ccc72763012a0134f8e790793
Vidhya307 <vidhyasukumar05@gmail.com>
2026-05-02 10:26:14 +0530

    Create ApiClient.java

---
+++ ApiClient.java
@@ -0,0 +1,24 @@
+package com.example.newsfeed;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+public class ApiClient {
+ private static final String BASE_URL = "https://newsapi.org/v2/";
+ private static Retrofit retrofit = null;
+ public static ApiInterface getInterface() {
+ if (retrofit == null) {
+ HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+ logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+OkHttpClient client = new OkHttpClient.Builder()
+ .addInterceptor(logging)
+ .build();
+ retrofit = new Retrofit.Builder()
+ .baseUrl(BASE_URL)
+ .addConverterFactory(GsonConverterFactory.create())
+ .client(client)
+ .build();
+ }
+ return retrofit.create(ApiInterface.class);
+ }
+}
